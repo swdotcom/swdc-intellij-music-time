f30980791b56129468f7ac9b6a20f01aa453c3b7
kkadu <kkadu@microproindia.com>
2020-02-05 17:47:57 +0530

    Tool window updated

---
+++ src/com/softwareco/intellij/plugin/music/PlaylistMouseListener.java
@@ -0,0 +1,90 @@
+package com.softwareco.intellij.plugin.music;
+
+import com.softwareco.intellij.plugin.SoftwareCoUtils;
+import com.softwareco.intellij.plugin.actions.MusicToolWindow;
+
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class PlaylistMouseListener extends MouseAdapter {
+    PlaylistTree playlistTree;
+
+    public PlaylistMouseListener(PlaylistTree playlistTree) {
+        this.playlistTree = playlistTree;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        super.mouseClicked(e);
+
+        PlaylistTreeNode node = (PlaylistTreeNode)
+                playlistTree.getLastSelectedPathComponent();
+
+        /* if nothing is selected */
+        if (node == null || node.getId() == null) return;
+
+        boolean activate = false;
+        if(MusicControlManager.currentTrackName == null && MusicControlManager.spotifyDeviceIds.size() > 0) {
+            activate = MusicControlManager.activateDevice(MusicControlManager.spotifyDeviceIds.get(0));
+            SoftwareCoUtils.updatePlayerControles();
+        }
+
+        /* retrieve the node that was selected */
+        if(node.isLeaf()) {
+            PlaylistTreeNode root = (PlaylistTreeNode) node.getRoot();
+            if(root.getId().equals(MusicControlManager.currentPlaylistId)
+                    && node.getId().equals(MusicControlManager.currentTrackId) && MusicControlManager.currentTrackName != null) {
+                if(!activate) {
+                    if (MusicControlManager.defaultbtn.equals("pause"))
+                        PlayerControlManager.pauseSpotifyDevices();
+                    else if (MusicControlManager.defaultbtn.equals("play"))
+                        PlayerControlManager.playSpotifyDevices();
+                }
+            } else {
+                if(!activate && MusicControlManager.currentTrackName == null &&
+                        (MusicControlManager.playerType.equals("Web Player") || !SoftwareCoUtils.isSpotifyRunning())) {
+                    MusicControlManager.launchPlayer();
+                    MusicToolWindow.lazilyCheckPlayer(20, root.getId(), node.getId());
+                } else {
+                    PlayerControlManager.playSpotifyPlaylist(root.getId(), node.getId());
+                }
+            }
+        } else {
+            if(node.getId().equals(MusicControlManager.currentPlaylistId) && MusicControlManager.currentTrackName != null) {
+                if(!activate) {
+                    if (MusicControlManager.defaultbtn.equals("pause"))
+                        PlayerControlManager.pauseSpotifyDevices();
+                    else if (MusicControlManager.defaultbtn.equals("play"))
+                        PlayerControlManager.playSpotifyDevices();
+                }
+            } else {
+                PlaylistTreeNode child = (PlaylistTreeNode) node.getFirstChild();
+
+                if(child.getId() != null) {
+                    if (!activate && MusicControlManager.currentTrackName == null &&
+                            (MusicControlManager.playerType.equals("Web Player") || !SoftwareCoUtils.isSpotifyRunning())) {
+                        MusicControlManager.launchPlayer();
+                        MusicToolWindow.lazilyCheckPlayer(20, node.getId(), child.getId());
+                    } else {
+                        PlayerControlManager.playSpotifyPlaylist(node.getId(), child.getId());
+                    }
+                } else {
+                    SoftwareCoUtils.showMsgPrompt("Expand Playlist to load tracks");
+                }
+            }
+        }
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        super.mouseEntered(e);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        super.mouseExited(e);
+        JTree tree=(JTree) e.getSource();
+        tree.clearSelection();
+    }
+}
