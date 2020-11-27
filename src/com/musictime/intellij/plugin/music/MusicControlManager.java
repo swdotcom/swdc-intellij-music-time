018619bbf6bfbd0ab3a4478de7ff6db210d6bf5d
Xavier Luiz <xavluiz@gmail.com>
2020-11-26 19:03:47 -0800

    updated token to plugin_token

--- src/com/musictime/intellij/plugin/music/MusicControlManager.java
+++ src/com/musictime/intellij/plugin/music/MusicControlManager.java
@@ -108,7 +108,7 @@ public class MusicControlManager {
          *     const endpoint = `${api_endpoint}/auth/spotify?${qryStr}`;
          */
         String jwt = FileManager.getItem("jwt");
-        String api = Client.api_endpoint + "/auth/spotify?token=" + jwt + "&mac=" + SoftwareCoUtils.isMac();
+        String api = Client.api_endpoint + "/auth/spotify?plugin_token=" + jwt + "&mac=" + SoftwareCoUtils.isMac();
         BrowserUtil.browse(api);
     }
 
